fd174f08e9a0ed820219317136fcecd180f6c564
Marian <mariano.anino96@gmail.com>
2020-02-17 16:04:37 -0300

    test com

--- src/main/java/com/manino/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/manino/springboot/app/productos/controllers/ProductoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.manino.springboot.app.productos.models.entity.Producto;
@@ -13,6 +14,7 @@ import com.manino.springboot.app.productos.models.service.IProductoService;
 
 @RestController
 public class ProductoController {
+
 	
 	@Autowired
 	private IProductoService productoService;
